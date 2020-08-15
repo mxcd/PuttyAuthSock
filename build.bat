db3b1a0e1e55c9691fa6a704af789067ad65ecf4
Max Partenfelder <max.partenfelder@wilde-it.com>
2020-08-15 17:10:47 +0200

    fixed automatic build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 python -m venv venv
-venv/Scripts/activate.bat
+CALL venv/Scripts/activate.bat
 pip install -r requirements.txt
 pyinstaller --onefile --icon=icon.ico --add-data "icon.png;files" --add-data "icon_green.png;files" --noconsole -n PuttyAuthSock main.py
